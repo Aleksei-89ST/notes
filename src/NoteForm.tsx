f17bf0ba5f5775f4c88b8177aae8a38336fc72cf
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-12-05 16:41:46 +0400

    logic Note

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,28 +1,41 @@
-import { FormEvent, useRef, useState } from "react";
-import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import CreatableReactSelect from "react-select/creatable";
-import { NoteData, Tag } from "./App";
+import { FormEvent, useRef, useState } from "react"
+import { Button, Col, Form, Row, Stack } from "react-bootstrap"
+import { Link, useNavigate } from "react-router-dom"
+import CreatableReactSelect from "react-select/creatable"
+import { NoteData, Tag } from "./App"
+import { v4 as uuidV4 } from "uuid"
 
 type NoteFormProps = {
-  onSubmit:(data: NoteData) => void
-}
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
+} & Partial<NoteData>
 
-
-export const NoteForm = ({onSubmit}:NoteFormProps ) => {
-  const titleRef = useRef<HTMLInputElement>(null);// состояние для title
-  const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags,setSelectedTags] = useState<Tag[]>([])
+export function NoteForm({
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = "",
+  markdown = "",
+  tags = [],
+}: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null)
+  const markdownRef = useRef<HTMLTextAreaElement>(null)
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags)
+  const navigate = useNavigate()
 
   function handleSubmit(e: FormEvent) {
-e.preventDefault()
+    e.preventDefault()
 
-onSubmit({
-  title:titleRef.current!.value,
-  markdown:markdownRef.current!.value,
-  tags:[],
-})
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: selectedTags,
+    })
+
+    navigate("..")
   }
+
   return (
     <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
@@ -30,39 +43,57 @@ onSubmit({
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control ref={titleRef} required defaultValue={title} />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreatableReactSelect value={selectedTags.map((tag) => {
-                return {label: tag.label,value:tag.id}
-              })} 
-              onChange={tags => {
-                setSelectedTags(tags.map(tag => {
-                  return {label: tag.label,id:tag.value}
-                }))
-              }}
-              isMulti />
+              <CreatableReactSelect
+                onCreateOption={label => {
+                  const newTag = { id: uuidV4(), label }
+                  onAddTag(newTag)
+                  setSelectedTags(prev => [...prev, newTag])
+                }}
+                value={selectedTags.map(tag => {
+                  return { label: tag.label, value: tag.id }
+                })}
+                options={availableTags.map(tag => {
+                  return { label: tag.label, value: tag.id }
+                })}
+                onChange={tags => {
+                  setSelectedTags(
+                    tags.map(tag => {
+                      return { label: tag.label, id: tag.value }
+                    })
+                  )
+                }}
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea"ref={markdownRef} rows={15} />
+          <Form.Control
+            defaultValue={markdown}
+            required
+            as="textarea"
+            ref={markdownRef}
+            rows={15}
+          />
         </Form.Group>
-        <Stack direction="horizontal" gap={2}>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
             Save
           </Button>
-          <Link to={".."}>
-          <Button type="button" variant="outline-secondary">
-            Cancel
-          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
           </Link>
         </Stack>
       </Stack>
     </Form>
-  );
-};
+  )
+}
